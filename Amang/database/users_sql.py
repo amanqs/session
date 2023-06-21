694a346db07f12a319feca46ad22bf9341164a68
amanqs <116822442+amanqs@users.noreply.github.com>
2023-06-21 20:48:59 +0700

    Update users_sql.py

--- Amang/database/users_sql.py
+++ Amang/database/users_sql.py
@@ -1,35 +1,35 @@
 from env import DATABASE_URL
 
-from sqlalchemy import Column, BigInteger, create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, BigInteger
 
-engine = create_engine(DATABASE_URL)
-BASE = declarative_base(bind=engine)
-SESSION = scoped_session(sessionmaker(bind=engine))
+if DATABASE_URL !="":
+    from Amang.database import BASE, SESSION
+else:
+    BASE = object
 
 
 class Users(BASE):
-    __tablename__ = "users"
-    user_id = Column(BigInteger, primary_key=True)
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    user_id = Column(BigInteger, primary_key=True)
 
-    def init(self, user_id, channels=None):
-        if DATABASE_URL == "":
-            return
-        self.user_id = user_id
-        self.channels = channels
+    def __init__(self, user_id, channels=None):
+        if DATABASE_URL == "":
+            return
+        self.user_id = user_id
+        self.channels = channels
 
-    # def repr(self):
-    #     return "".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
+    # def __repr__(self):
+    #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
 
 
-if DATABASE_URL != "":
-    Users.table.create(checkfirst=True)
+if DATABASE_URL !="":
+    Users.__table__.create(checkfirst=True)
 
 
 async def num_users():
-    if DATABASE_URL != "":
-        try:
-            return SESSION.query(Users).count()
-        finally:
-            SESSION.close()
+    if DATABASE_URL !="":
+        try:
+            return SESSION.query(Users).count()
+        finally:
+            SESSION.close()
