c394b0fc86b58d9c604465aeb397dd46a4043abb
amanqs <116822442+amanqs@users.noreply.github.com>
2023-10-30 20:09:19 +0700

    Update env.py

--- env.py
+++ env.py
@@ -1,13 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(.env)
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
-MUST_JOIN = os.getenv("MUST_JOIN", "kynansupport")
+API_ID = os.getenv("API_ID", "22377432").strip()
+API_HASH = os.getenv("API_HASH", "69dbb3a23605d52444caf76c39631dd6").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6362933544:AAE4S_QQ5KnOJgkxD1DHme2Yc0He0CVvE_g").strip()
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://ilxnvuj833:0212@cluster0.ile0pdo.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = os.getenv("MUST_JOIN", "amwangs")
 
 if not API_ID:
     raise SystemExit("No API_ID found. Exiting...")
@@ -25,6 +25,3 @@ try:
     API_ID = int(API_ID)
 except ValueError:
     raise SystemExit("API_ID is not a valid integer. Exiting...")
-
-if 'postgres' in DATABASE_URL and 'postgresql' not in DATABASE_URL:
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
