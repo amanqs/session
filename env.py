e83569fc94f62d11b40d3421a7ac8dff850da4b9
amanqs <116822442+amanqs@users.noreply.github.com>
2023-10-30 20:15:34 +0700

    Update env.py

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID", "22377432").strip()
 API_HASH = os.getenv("API_HASH", "69dbb3a23605d52444caf76c39631dd6").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "6362933544:AAE4S_QQ5KnOJgkxD1DHme2Yc0He0CVvE_g").strip()
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://ilxnvuj833:0212@cluster0.ile0pdo.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = getenv("OWNER_ID", "1839010591")
+OWNER_ID = os.getenv("OWNER_ID", "1839010591")
 MUST_JOIN = os.getenv("MUST_JOIN", "amwangs")
 
 if not API_ID:
